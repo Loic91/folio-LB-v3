8bfcd616f7b3050ef33d849abe775f36beec9269
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-06-15 01:08:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,172 @@
+TweenMax.to(".block_1", 3.5, {
+    x: "-180",
+    y: "-100",
+    scale: "2.4",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_2", 3.5, {
+    x: "-180",
+    y: "200",
+    scale: "1.2",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_3", 3.5, {
+    x: "180",
+    y: "-240",
+    scale: "1.6",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_4", 3.5, {
+    x: "280",
+    y: "240",
+    scale: "0.8",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_5", 3.5, {
+    x: "380",
+    y: "140",
+    scale: "1.3",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_6", 3.5, {
+    x: "500",
+    y: "200",
+    scale: "2.4",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_7", 3.5, {
+    x: "-220",
+    y: "150",
+    scale: "1.2",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_8", 3.5, {
+    x: "220",
+    y: "-150",
+    scale: "1.6",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_9", 3.5, {
+    x: "-100",
+    y: "150",
+    scale: "0.5",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_10", 3.5, {
+    x: "-500",
+    y: "-100",
+    scale: "0.9",
+    ease: Expo.easeInOut,
+});
+
+TweenMax.to(".block_11", 3.5, {
+    x: "-120",
+    y: "80",
+    scale: "0.5",
+    ease: Expo.easeInOut,
+});
+
+
+
+
+
+// TweenMax.to(".box", 2.4, {
+//     y: "-100%",
+//     ease: Expo.easeInOut,
+// });
+
+TweenMax.from(".circle_shape", 3.9, {
+    scale: "0",
+    ease: Expo.easeInOut,
+});
+// TweenMax.from(".circle_shape_2", 2.4, {
+//     scale: "0",
+//     ease: Expo.easeInOut,
+// });
+// TweenMax.from(".circle_shape_3", 2.4, {
+//     scale: "0",
+//     ease: Expo.easeInOut,
+// });
+TweenMax.from(".navbar > div", 1.6, {
+    opacity: 0,
+    y: 60,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+});
+TweenMax.from(".site_logo", 1.6, {
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+});
+TweenMax.from(".showreel", 1.6, {
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+});
+TweenMax.staggerFrom(
+    ".site_menu > div",
+    1,
+    {
+        opacity: 0,
+        y: 60,
+        ease: Power2.easeOut,
+    },
+    0.2
+);
+
+
+
+TweenMax.from("#front__1", 2.6, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 2.8,
+});
+TweenMax.from("#dev__1", 2.6, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 3.6,
+});
+
+TweenMax.from("#front__1__mobile", 2.6, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 2.8,
+});
+TweenMax.from("#front__1__mobile2", 2.6, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 3.6,
+});
+TweenMax.from("#dev__1__mobile", 2.6, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 4,
+});
+TweenMax.from("#dev__1__mobile2", 2.6, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 4.4,
+});
+TweenMax.from("#dev__1__mobile3", 2.6, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 4.8,
+});
